e27e0eb81b0ced69e3828c01be9715ba1b561e10
mikey1977 <mmi@umich.edu>
2015-09-26 16:31:41 -1000

    new instance of Meal with 'cereal and milk'

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1150,7 +1150,7 @@ var chocolateChip = new Cookie('chocolate');
 var gingerbread = new Cookie('gingerbread');
 
 // Create 2 different meals
-var breakfast;
+var breakfast = new Meal('cereal and milk');
 var dinner;
 
 
