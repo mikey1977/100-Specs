0e8aed170db838a04c0b3b43bc408272c340c9c4
mikey1977 <mmi@umich.edu>
2015-09-26 15:34:57 -1000

    Shoe class created with 'size' and 'color' properties

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1061,7 +1061,10 @@ function Door(isOpen) {
  * @param {number} size  The shoe size
  * @param {string} color The shoe color
  */
-
+function Shoe(size, color) {
+  this.size = size;
+  this.color = color;
+}
 
 /**
  * Step 56
