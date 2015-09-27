c4ff3752cf83a05bd295a59e2348afc7c4311efd
mikey1977 <mmi@umich.edu>
2015-09-26 18:22:55 -1000

    openClose method on Door class opens door if closed, closes door if open

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1286,7 +1286,15 @@ Box.prototype.openBox = function() {
  * Return true if openClose opens the door, false if openClose closes the door.
  *
  */
-
+Door.prototype.openClose = function() {
+  if (this.isOpen === false) {
+    this.isOpen = true;
+    return true;
+  } else {
+    this.isOpen = false;
+    return false;
+  }
+}
 
 /* Step 86
  *
